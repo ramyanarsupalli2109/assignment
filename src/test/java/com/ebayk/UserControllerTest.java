6952d70ad882ba90ad21474c99ef172c623fb53f
Ramya Narsupalli <ramya.narsupalli2109@gmail.com>
2022-04-23 19:49:32 +0530

    added test

---
+++ src/test/java/com/ebayk/UserControllerTest.java
@@ -0,0 +1,20 @@
+package com.ebayk;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.ebayk.data.user.User;
+import java.net.URL;
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+	  @Test
+	    void listAd(Integer userID) {
+		        UserController userController = new UserController();
+			    ListAd listAdResponse = userController.listAd(userID);
+			        
+			        assertNotNull(listAdResponse.getID());
+				    assertDoesNotThrow(() -> new URL(listAdResponse.getImageUrl()));
+				      }
+}
